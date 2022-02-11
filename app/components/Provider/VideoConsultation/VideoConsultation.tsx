a2294246d7b4e7f47aad0dc2f3ac4e72cc2def9f
Alexey Kurasov <akurasov@greenm.org>
2022-02-11 14:26:42 +0200

    fix

--- app/components/Provider/VideoConsultation/VideoConsultation.tsx
+++ app/components/Provider/VideoConsultation/VideoConsultation.tsx
@@ -23,7 +23,6 @@ import { TelehealthVisit } from '../../../types';
 
 export interface VideoConsultationProps {}
 
-const providerName = 'Dr. Josefina Santos';
 
 export const VideoConsultation = ({}: VideoConsultationProps) => {
   const [isAudioEnabled, toggleAudioEnabled] = useLocalAudioToggle();
@@ -65,7 +64,7 @@ export const VideoConsultation = ({}: VideoConsultationProps) => {
           ...prev,
           providerParticipant: room!.localParticipant,
           patientParticipant: participants.find(p => p.identity != room!.localParticipant.identity),
-          visitorParticipant: participants[1]
+          visitorParticipant: participants.find(p => p.identity.includes('visitor_') )
         }
       })
     }
@@ -94,7 +93,7 @@ export const VideoConsultation = ({}: VideoConsultationProps) => {
         <div className="absolute right-6 min-w-[12rem] w-[15%] h-[16%] flex flex-col z-20">
           {callState.providerParticipant &&
             <VideoParticipant
-              name={providerName}
+              name={visit.ehrProvider.name}
               hasAudio={isAudioEnabled}
               hasVideo={isVideoEnabled}
               isProvider
@@ -107,7 +106,6 @@ export const VideoConsultation = ({}: VideoConsultationProps) => {
               name='Invited Visitor'
               hasAudio={isAudioEnabled}
               hasVideo={isVideoEnabled}
-              isSelf
               participant={callState.visitorParticipant}
               fullScreen
             />}
@@ -116,7 +114,7 @@ export const VideoConsultation = ({}: VideoConsultationProps) => {
         <div className="w-2/3 h-full">
             {mainDisplayedParticipant &&
               <VideoParticipant
-                name="Sarah Coopers"
+                name={`${visit.ehrPatient.given_name} ${visit.ehrPatient.family_name}`}
                 hasAudio
                 hasVideo
                 participant={mainDisplayedParticipant}
